f4850510865dd61118ac6c81f66836c8c71a380c
Allan Mercou <allan.mercou@icloud.com>
2020-07-05 23:31:53 +0200

    Adds the Concat for two spans. Thanks Nrik.

---
+++ src/IRCSharp/MemoryHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IRCSharp
+{
+    public static class MemoryHelpers
+    {
+        public static ReadOnlyMemory<T> Concat<T>(ReadOnlyMemory<T> left, ReadOnlyMemory<T> right)
+        {
+            Memory<T> memory = new T[left.Length + right.Length];
+            
+            left.CopyTo(memory[..left.Length]);
+            right.CopyTo(memory[left.Length..]);
+
+            return memory;
+        }
+    }
+}
